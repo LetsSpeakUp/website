850cc3effc1593476dca398688f13cfad40545f2
Pranav Tripathi <tripathipranav14@gmail.com>
2023-03-09 19:17:04 +0530

    Add home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home'
 import './App.css'
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
-            
+            <Route index element={<Home />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+        <h1>
+            Hello from home
+        </h1>
+    )
+}
+
+export default Home
