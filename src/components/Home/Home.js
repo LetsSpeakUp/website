c96b19d0c50f6d252871579a59c26648658373ea
Pranav Tripathi <tripathipranav14@gmail.com>
2023-03-10 10:06:44 +0530

    Add home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
 import './Home.css'
+import logoAlt from '../../images/logoAlt.png'
 
 const Home = () => {
-    return(
-        <h1>
-            Hello from home
-        </h1>
+    return (
+        <div className='head-container'>
+            <div className='top'>
+                <div>
+                    <h2 className='head-quote'>
+                        Empowering individuals, inspiring change; Building better personalities together.
+                    </h2>
+                    <a href='#events'><button className='event-button'>Get Started</button></a>
+                </div>
+                <img className='logo-img' src={logoAlt} alt='logo' />
+            </div>
+        </div>
+
     )
 }
 
