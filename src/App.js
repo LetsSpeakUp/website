b7bc61e7615c1fb03da4b2d95a00399f820d4133
Pranav Tripathi <tripathipranav14@gmail.com>
2023-04-04 22:06:34 +0530

    Folder name change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
-import About from './components/about/About'
+import About from './components/About/About'
 
 const App = () => {
   return (
