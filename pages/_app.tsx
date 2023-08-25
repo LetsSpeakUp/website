7b5f8cda469f4042c6a3fdbd16030c1bb5e24d01
Ishaan <ishaan2053@gmail.com>
2023-08-25 13:06:04 +0530

    Shifted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
-import "tw-elements/dist/css/tw-elements.min.css";
 
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
